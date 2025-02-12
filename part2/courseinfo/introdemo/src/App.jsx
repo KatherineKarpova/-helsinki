7809e76e2940a34b7d4c3238c3b0012dd632fe07
Katherine Karpova <katherinekarpova922@gmail.com>
2025-02-12 14:21:16 -0500

    2.4 step 9

--- part2/courseinfo/introdemo/src/App.jsx
+++ part2/courseinfo/introdemo/src/App.jsx
@@ -1,53 +1,76 @@
 const App = () => {
   console.log('app works')
   
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4
-      }
-    ]
-  }
-
-  // Define a component responsible for formatting a single course called Course.
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+  console.log('Courses array before rendering:', courses)
   const Course = ({course}) => {
     return (
       // header
       // contents as part name and num exercises each
       <div>
         <h1>{course.name}</h1>
-        <p>
+        <>
           {course.parts.map(part => (
             <p key={part.name}>{part.name} {part.exercises} </p>
           ))}
-        </p>
+        </>
         <p>
-          Total number of exercises {course.parts.reduce((sum, part) => sum + part.exercises, 0)}
+          <strong>total of {course.parts.reduce((sum, part) => sum + part.exercises, 0)} exercises</strong>
         </p>
       </div>
       )
     }
 
-  return <Course course={course} />
+  return (
+    
+    <div>
+      {courses.map((course) => (
+        <Course key={course.id} course={course}/>
+    ))}
+    </div>
+  )
 }
-
 export default App
