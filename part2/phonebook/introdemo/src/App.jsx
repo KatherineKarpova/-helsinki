d5788e938b522a04d40a62b9ce925bd0df800357
Katherine Karpova <katherinekarpova922@gmail.com>
2025-02-13 12:38:00 -0500

    2.8 step 3

--- part2/phonebook/introdemo/src/App.jsx
+++ part2/phonebook/introdemo/src/App.jsx
@@ -3,24 +3,36 @@ import Person from './components/Person'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const handleNewName = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
   }
 
+  const handleNewNumber = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
+
   // add new name to the persons array
   // copy the array first do not change it directly
-  const addNewName = (event) => {
+  const addNewPerson = (event) => {
     event.preventDefault()
     console.log('button clicked', event.target)
     // create new name as a seperate object to check and set
-    const nameObject = {
+    const personObject = {
       name: newName,
+      number: newNumber,
+      id: persons.length + 1,
     }
+    console.log('new id:', personObject.id)
     // check if the new name is already in the persons array
     const nameExists = persons.some(person => person.name === newName)
     // alert message to user if true/match found
@@ -29,22 +41,21 @@ const App = () => {
     }
     else {
       // copy array and append new name if false
-      setPersons(persons.concat(nameObject))
+      setPersons(persons.concat(personObject))
     }
     // reset to the default before event handler
     setNewName('')
+    setNewNumber('')
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addNewName}>
+      <form onSubmit={addNewPerson}>
         <div>
-          name: <input 
-          value={newName}
-          onChange={handleNewName}
-          />
+          name: <input value={newName} onChange={handleNewName}/>
         </div>
+        <div>number: <input value={newNumber} onChange={handleNewNumber}/></div>
         <div>
           <button type="submit">add</button>
         </div>
@@ -52,7 +63,7 @@ const App = () => {
       <h2>Numbers</h2>
       <div>
         {persons.map(person => 
-        <Person key={person.name} person={person}/>
+        <Person key={person.id} person={person}/>
         )}
       </div>
     </div>
